649ad2111034b1aa549a76dddad91a995e17d4d6
Danchi-1 <ohachordaniel2021@gmail.com>
2026-03-18 16:00:32 +0100

    feat: completed first class

---
+++ class_one.py
@@ -0,0 +1,56 @@
+print("Hello, World!")
+name = "Marvellous"
+print(f"Hello, {name}!")
+x = 10
+y = 3.14
+name = "Daniel"
+is_student = True
+numbers = [1, 2, 3, 4, 5]
+person = {
+    "name": "Daniel", 
+    "age": 25
+}
+
+set1 = {1, "Daniel", "Apple", True, 5.23}
+list1 = [1, "Daniel", "Apple", True, 5.23]
+# print(f"list1: {list1}")
+tuple1 = (1, "Daniel", "Apple", True, 5.23)
+
+list1.append("New Item")
+tuple1 += ("New Item",)
+print(f"tuple1: {tuple1}")
+# print(list1)
+
+# Data types in Python:
+# integer
+# float
+# string -> ""
+# boolean -> True or False
+# list -> []
+# tuple -> ()
+# dictionary -> {}
+# set -> {}
+
+
+
+
+
+
+
+
+
+# def greet(name):
+#     return f"Hello, {name}!"
+
+# print(greet("Daniel"))
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def introduce(self):
+#         return f"My name is {self.name} and I am {self.age} years old."
+
+# person = Person("Daniel", 25)
+# print(person.introduce())
